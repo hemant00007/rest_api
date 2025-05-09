3da892e4daaf4ae256cce0e151f5604e3d1dca67
hemant <hemantkumar692@gmail.com>
2025-05-09 11:50:50 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const app = require('./app'); // Import the app module
+const server = http.createServer(app);
+
+ server.listen(300,console.log('Server is running on port 3000'));
+// server.listen(300, () => {
+//     console.log('Server is running on port 300');
+//   });
