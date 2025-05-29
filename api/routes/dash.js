98e39c48818263832d6b4e78043e54b5bc87d872
hemant <hemantkumar692@gmail.com>
2025-05-29 12:42:39 +0530

    forth commit

--- api/routes/dash.js
+++ api/routes/dash.js
@@ -5,6 +5,7 @@ router.get("/", (req, res, next) => {
   res.status(200).json({
     status: 200,
     msg: "this is dashboard page",
+    time: "Time is 12:42 pm",
   });
 });
 module.exports = router;
