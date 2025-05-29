2f0396b304ba781c1e366f74dfcaf21da113f6f4
hemant <hemantkumar692@gmail.com>
2025-05-29 11:53:20 +0530

    new commit

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,120 +1,119 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Product = require('../model/productmodel');
-const mongoose = require('mongoose');
-
+const Product = require("../model/productmodel");
+const mongoose = require("mongoose");
 
 // router.get('/list') get request
-router.get('/',(req,res,next) => {
-    Product.find()
-    .then(result => {
-        res.status(200).json({
-            status:true,
-            message: 'Product fetched successfully',
-            data: result
-        })
+router.get("/", (req, res, next) => {
+  Product.find()
+    .then((result) => {
+      res.status(200).json({
+        status: true,
+        message: "Product fetched successfully",
+        data: result,
+      });
     })
-    .catch(err => {
-        res.status(500).json({
-            status:false,
-            message: 'Error fetching products',
-            error: err
-        })
-    })
-})
-// this is a post request 
-router.post('/',(req,res,next) => {
- const product = new Product({
+    .catch((err) => {
+      res.status(500).json({
+        status: false,
+        message: "Error fetching products",
+        error: err,
+      });
+    });
+});
+// this is a post request
+router.post("/", (req, res, next) => {
+  const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price,
     productImage: req.body.productImage,
-    description: req.body.description
-})
- product.save()
- .then(result=> {
+    description: req.body.description,
+  });
+  product.save().then((result) => {
     console.log(result);
     res.status(200).json({
-        status:true,
-        message: 'Product created successfully',})
- })
-})
+      status: true,
+      message: "Product created successfully",
+    });
+  });
+});
 // get request for a specific product
-router.get('/:productId',(req,res,next) => {
-    const id = req.params.productId;
-    console.log(id);
-    Product.findById(id)
-    .then(result => {
-        if(result){
-            res.status(200).json({
-                status:true,
-                message: 'Product fetched successfully',
-                data: result
-            })
-        }else{
-            res.status(404).json({
-                status:false,
-                message: 'Product not found'
-            })
-        }
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  console.log(id);
+  Product.findById(id)
+    .then((result) => {
+      if (result) {
+        res.status(200).json({
+          status: true,
+          message: "Product fetched successfully",
+          data: result,
+        });
+      } else {
+        res.status(404).json({
+          status: false,
+          message: "Product not found",
+        });
+      }
     })
-    .catch(err => {
-        res.status(500).json({
-            status:false,
-            message: 'Error fetching product',
-            error: err
-        })
-    })
-})
+    .catch((err) => {
+      res.status(500).json({
+        status: false,
+        message: "Error fetching product",
+        error: err,
+      });
+    });
+});
 
 // this is a delete request
-router.delete('/:productId',(req,res,next) => {
-    const id = req.params.productId;
-    Product.deleteOne({_id: id})
-    .then(result => {
-        res.status(200).json({
-            status:true,
-            message: 'Product deleted successfully',
-            data: result
-        })
+router.delete("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  Product.deleteOne({ _id: id })
+    .then((result) => {
+      res.status(200).json({
+        status: true,
+        message: "Product deleted successfully",
+        data: result,
+      });
     })
-    .catch(err => {
-        res.status(500).json({
-            status:false,
-            message: 'Error deleting product',
-            error: err
-        })
-    })
-})
+    .catch((err) => {
+      res.status(500).json({
+        status: false,
+        message: "Error deleting product",
+        error: err,
+      });
+    });
+});
 // this is a put request
-router.put('/:productId',(req,res,next) => {
-    const id = req.params.productId;
-   
-    Product.findOneAndUpdate({_id: id},
-        
-        {
-        $set: {
-             name: req.body.name,
-            price: req.body.price,
-            productImage: req.body.productImage,
-            description: req.body.description
-        }
-    })
-    .then(result => {
-        res.status(200).json({
-            status:true,
-            message: 'Product updated successfully',
-           
-        })
-    })
-    .catch(err => {
-        res.status(500).json({
-            status:false,
-            message: 'Error updating product',
-            error: err
-        })
-    })
-})
+router.put("/:productId", (req, res, next) => {
+  const id = req.params.productId;
 
+  Product.findOneAndUpdate(
+    { _id: id },
+
+    {
+      $set: {
+        name: req.body.name,
+        price: req.body.price,
+        productImage: req.body.productImage,
+        description: req.body.description,
+      },
+    }
+  )
+    .then((result) => {
+      res.status(200).json({
+        status: true,
+        message: "Product updated successfully",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: false,
+        message: "Error updating product",
+        error: err,
+      });
+    });
+});
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const facultys = require('./api/routes/faculty');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const productRoute = require('./api/routes/product')
-const home = require('./api/routes/home');
-   const userRoute = require('./api/routes/user');
-   const welcome = require('./api/routes/welcome');
+const facultys = require("./api/routes/faculty");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const productRoute = require("./api/routes/product");
+const home = require("./api/routes/home");
+const userRoute = require("./api/routes/user");
+const welcome = require("./api/routes/welcome");
+const dash = require("./api/routes/dash");
 
-mongoose.connect('mongodb+srv://hemantkumar692:u8G7GR8vq709XaJk@cluster0.z5upxof.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
-mongoose.connection.on('error', err => {
-    console.log('Error connecting to MongoDB:', err);
+mongoose.connect(
+  "mongodb+srv://hemantkumar692:u8G7GR8vq709XaJk@cluster0.z5upxof.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to MongoDB:", err);
 });
-mongoose.connection.on('connected', () => {
-    console.log('Connected to MongoDB');
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
 });
 
 // use body parser to parse incoming request bodies (urlencoded and json) before the routes
@@ -21,18 +24,16 @@ mongoose.connection.on('connected', () => {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-app.use('/',home);
-app.use('/faculty',facultys);
-app.use('/product',productRoute)
-app.use('/user',userRoute);
-app.use('/',welcome);
-
-
+app.use("/", home);
+app.use("/faculty", facultys);
+app.use("/product", productRoute);
+app.use("/user", userRoute);
+app.use("/", welcome);
+app.use("/dashboard", dash);
 
 app.use((req, res, next) => {
-    res.status(404).json({
-       error: 'Not found'
-    })
+  res.status(404).json({
+    error: "Not found",
+  });
 });
 module.exports = app;
